49d7471f86a4c8cb1e0d9c958cbece8ccea6c9f7
cipri.sanchez <cipri.sanchez@gmail.com>
2022-03-01 23:12:11 +0100

    Add ignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module com.craneos.java11{
-    exports com.craneos.java11.part1.chapter1;
     exports com.craneos.java11.part1.chapter2.blocks;
     exports com.craneos.java11.part1.chapter1.examples;
 }
